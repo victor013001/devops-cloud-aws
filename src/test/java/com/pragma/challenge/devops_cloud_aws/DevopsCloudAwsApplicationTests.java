c3d276977e9385a93f4f8b851876a32d8bfe968b
victor013001 <victormanuelosoriog@gmail.com>
2025-05-02 16:54:35 -0500

    fix: configure testcontainer with R2DBC

--- src/test/java/com/pragma/challenge/devops_cloud_aws/DevopsCloudAwsApplicationTests.java
+++ src/test/java/com/pragma/challenge/devops_cloud_aws/DevopsCloudAwsApplicationTests.java
@@ -1,15 +1,51 @@
 package com.pragma.challenge.devops_cloud_aws;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
 
-@Import(TestcontainersConfiguration.class)
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class DevopsCloudAwsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    static MySQLContainer<?> container = TestcontainersConfiguration.mysqlContainer();
+
+    @BeforeAll
+    static void beforeAll() {
+        container.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        container.stop();
+    }
+
+    @DynamicPropertySource
+    static void datasourceProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", () ->
+                String.format("r2dbc:mysql://%s:%d/%s",
+                        container.getHost(),
+                        container.getFirstMappedPort(),
+                        container.getDatabaseName()));
+        registry.add("spring.r2dbc.username", container::getUsername);
+        registry.add("spring.r2dbc.password", container::getPassword);
+        registry.add("spring.flyway.url", () ->
+                String.format("jdbc:mysql://%s:%d/%s",
+                        container.getHost(),
+                        container.getFirstMappedPort(),
+                        container.getDatabaseName()));
+        registry.add("spring.flyway.user", container::getUsername);
+        registry.add("spring.flyway.password", container::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+        assertThat(container.isCreated()).isTrue();
+    }
 
 }
--- src/test/java/com/pragma/challenge/devops_cloud_aws/TestcontainersConfiguration.java
+++ src/test/java/com/pragma/challenge/devops_cloud_aws/TestcontainersConfiguration.java
@@ -1,18 +1,14 @@
 package com.pragma.challenge.devops_cloud_aws;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
-	}
+    static MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>("mysql:8")
+                .withDatabaseName("it_db")
+                .withUsername("admin")
+                .withPassword("admin");
+    }
 
 }
